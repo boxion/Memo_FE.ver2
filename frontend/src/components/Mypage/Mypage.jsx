389c49f2e864e6cd3511fc5088e5f1d6880dd44d
Taeksin Kim <yoyo2521@naver.com>
2024-09-24 00:00:10 +0900

    사이드메뉴 정상작동

--- frontend/src/components/Mypage/Mypage.jsx
+++ frontend/src/components/Mypage/Mypage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import styled from "styled-components";
 import Config from "../Config/config";
 import audioIcon from "../../assets/images/audio.png";
@@ -169,8 +169,19 @@ const Mypage = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [videoList, setVideoList] = useState([]);
   const navigate = useNavigate();
+  const location = useLocation();
   const [categoryName, setCategoryName] = useState(localStorage.getItem("categoryName") || "최근 본 영상");
   localStorage.removeItem("categoryName");
+
+  // 쿼리 스트링에서 값을 추출하는 로직
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    const category = queryParams.get("category"); // 쿼리 스트링의 'category' 값 가져오기
+
+    if (category) {
+      setCategoryName(category);
+    }
+  }, [location.search]);
 
   // 1초마다 한번씩 리셋 트리거
   useEffect(() => {
@@ -186,7 +197,7 @@ const Mypage = () => {
     getVideoList(categoryName);
   }, [categoryName]);
 
-  // 비디오 목록을 서버에서 가져오는 함수
+  // 비디오 목록을 서버에서 가져오는 함수...
   const getVideoList = async (categoryName) => {
     try {
       if (categoryName === "최근 본 영상") {
@@ -214,9 +225,7 @@ const Mypage = () => {
         isLocked: video.isPublished === false, // isPublished가 false면 잠금
       }));
   
-      
       responseData.reverse(); // 내림차순으로 비디오 정렬
-
       setVideoList(responseData); // 받아온 데이터를 상태에 저장
     } catch (error) {
       console.error("에러 발생:", error);
